a1e4477c5bd8860be824bca6417ade8d5b7ca5b3
Matheus <matheuslsv7@gmail.com>
2022-07-06 21:39:17 -0300

    Criado o Crud da Loja de Games

---
+++ LojaDeGamesGeneration/LojaDeGames/src/main/java/com/generation/games/model/Produto.java
@@ -0,0 +1,69 @@
+package com.generation.games.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String nome;
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String produtora;
+	
+	@NotNull
+	private Double preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getProdutora() {
+		return produtora;
+	}
+
+	public void setProdutora(String produtora) {
+		this.produtora = produtora;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+}
