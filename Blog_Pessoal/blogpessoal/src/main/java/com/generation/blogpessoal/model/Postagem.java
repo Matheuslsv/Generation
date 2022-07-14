9039faf1386e6278e4c5d6241f37b78a993a2309
Matheus <matheuslsv7@gmail.com>
2022-07-14 12:35:34 -0300

    Adicionada a camada de testes

--- Blog_Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ Blog_Pessoal/blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -44,6 +44,10 @@ public class Postagem {
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
 	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+	
 
 	public long getId() {
 		return id;
